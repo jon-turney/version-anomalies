e70d0b52a47a5f24418fba0a610d768355351c3e
Jon Turney <jon.turney@dronecode.org.uk>
2017-10-06 20:26:25 +0100

    Initial commit

---
+++ historical-analysis.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import urllib.request
+import version
+
+def parse_setup_ini(contents):
+    s = {}
+
+    for l in contents.splitlines():
+        if l.startswith('@'):
+            p = l[2:]
+            s[p] = []
+        elif l.startswith('version:'):
+            v = l[9:]
+            s[p].append(v)
+
+    return s
+
+
+# read index, build list of setup.uni URLs
+urls = []
+index_url = "http://ctm.crouchingtigerhiddenfruitbat.org/pub/cygwin/circa/64bit/index.html"
+html = urllib.request.urlopen(index_url).read().decode()
+for l in html.splitlines():
+    m = re.search('<td>(http.*)</td>', l)
+    if m:
+        urls.append(m.group(1) + '/setup.ini')
+
+
+# for each setup.ini URL, fetch, parse and compare with previous
+prev = None
+for u in urls:
+    circa = re.search('(circa/.*)/setup.ini', u).group(1)
+    cache_fn = os.path.join('cache', u.replace('http://', '').replace(os.path.sep, '_'))
+
+    if not os.path.isfile(cache_fn):
+        (filename, headers) = urllib.request.urlretrieve(u, cache_fn)
+        print('fetching %s' % filename)
+    else:
+        filename = cache_fn
+#        print('%s from cache' % filename)
+
+    # parse it
+    with open(filename) as f:
+        curr = parse_setup_ini(f.read())
+
+    # look for versions which went backwards and packages which disappeared
+    if prev:
+        for k in curr:
+            if k not in prev:
+#                print("'%s' disappeared in %s" % (k, filename))
+                continue
+
+            if len(curr[k]) < 1:
+#                print("'%s' doesn't have any versions in %s" % (k, circa))
+                continue
+
+            vc = version.SetupVersion(curr[k][0])
+            vp = version.SetupVersion(prev[k][0])
+
+            if vc > vp:
+                print("'%s' version went backwards from '%s' to '%s' in %s" % (k, curr[k][0], prev[k][0], circa))
+                # don't report this again
+                prev[k] = curr[k]
+    else:
+    # first becomes previous
+        prev = curr
